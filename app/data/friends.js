c0cf8c9cb2f0a0729be0cd7fa203ce684724baf1
Chet Budhathoki <okchet@gmail.com>
2019-12-19 20:52:00 -0700

    Pictures updated to make it work

--- app/data/friends.js
+++ app/data/friends.js
@@ -4,8 +4,8 @@
 //initialize starting with 'dummy' data
 var friendList = [
     {
-      name: 'Lou',
-      profilePic: 'https://www.familychoiceawards.com/wp-content/uploads/2014/04/Wiggleless-Dog-Back-Brace-200-x-200.jpg',
+      name: 'Rabbit',
+      profilePic: 'https://photos.app.goo.gl/Bjm89YSnHRCAK46s7',
       scores: [
         "4",
         "5",
@@ -22,8 +22,8 @@ var friendList = [
   
   
     {
-      name: 'Lou',
-      profilePic: 'http://adogbreeds.com/wp-content/uploads/2014/08/Small-White-Pomeranian-Dog-Breeds-200x200.jpg',
+      name: 'Bore',
+      profilePic: 'https://photos.app.goo.gl/H3Wp9qwcw6FQCL6z5',
       scores: [
         "4",
         "5",
@@ -40,8 +40,8 @@ var friendList = [
   
   
       {
-      name: 'Lou',
-      profilePic: 'https://i.pinimg.com/236x/4c/05/5d/4c055dcd99404c8fd445fdc5194756cc--cutest-dogs-cute-puppies.jpg',
+      name: 'Bison',
+      profilePic: 'https://photos.app.goo.gl/Q2k9U43yp39WgTp58',
       scores: [
         "4",
         "5",
@@ -58,8 +58,8 @@ var friendList = [
   
   
       {
-      name: 'Lou',
-      profilePic: 'http://www.edailypost.com/wp-content/uploads/2012/06/Maltese-Cutest-Dog-Breeds.jpg',
+      name: 'Monkey',
+      profilePic: 'https://photos.app.goo.gl/5YoBXi9Dc7r8g5yv9',
       scores: [
         "4",
         "5",
